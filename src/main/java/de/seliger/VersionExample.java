b5a60cde21d9569fdf90aab348727e65d3969f91
Juergen Seliger <juergen.seliger@gmail.com>
2016-11-13 15:14:28 +0100

    code formatiert

--- src/main/java/de/seliger/VersionExample.java
+++ src/main/java/de/seliger/VersionExample.java
@@ -4,20 +4,20 @@ public class VersionExample {
 
     public static void main(String[] args) {
         Runtime.Version version = Runtime.version();
-        System.out.println("version.toString: "+version.toString());
-        System.out.println("major: " +version.major());
-        System.out.println("minor: " +version.minor());
-        System.out.println("security: "+version.security());
+        System.out.println("version.toString: " + version.toString());
+        System.out.println("major: " + version.major());
+        System.out.println("minor: " + version.minor());
+        System.out.println("security: " + version.security());
         version.build().ifPresentOrElse(
-                (s) -> System.out.println("build: "+s),
+                (s) -> System.out.println("build: " + s),
                 () -> System.out.println("no build info")
         );
         version.optional().ifPresentOrElse(
-                (s) -> System.out.println("optional: "+s),
+                (s) -> System.out.println("optional: " + s),
                 () -> System.out.println("no optional info")
         );
         version.pre().ifPresentOrElse(
-                (s) -> System.out.println("pre: "+s),
+                (s) -> System.out.println("pre: " + s),
                 () -> System.out.println("no pre info")
         );
     }
